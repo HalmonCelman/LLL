959b2a79b5a7bd8993d24735513ea7897074b70b
Halmon <halmoncelman@gmail.com>
2023-08-02 17:43:21 +0200

    First commit
    - examples for add,addi,sub,subi
    - implemented functions add,addi,seri,out

---
+++ LLL_functions.c
@@ -0,0 +1,129 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////                                                              LLL instructions - by KK                                                              //////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "LLL_functions.h"
+
+lll_err LLL_add(void){
+    lll_err inst_err;
+    inst_err.status=LLL_OK;
+
+    uint32_t lll_reg;
+    uint8_t lll_ovf=0;
+    uint16_t lll_sum=0;
+
+    #if LLL_DEBUG_MODE
+        lll_number=lll_get();
+        lll_send_info("add number: ",lll_number);
+        LLL_CHECK_REG(inst_err);
+        lll_send_info("add reg mode: ",LLL_REG_MODE);
+        lll_reg = LLL_load_reg_addr(LLL_REG_MODE);
+    #else
+        lll_number=lll_get();
+        lll_h8=lll_get();
+        lll_reg1=LLL_load_reg_addr(LLL_REG_MODE);
+    #endif
+
+    for(int i=0;i<lll_number;i++){
+        lll_sum += LLL_load_mem(lll_reg+i);
+        if(lll_sum > 255){
+            lll_sum -= 255;
+            lll_ovf=1;
+        }    
+    }
+
+    #if LLL_DEBUG_MODE
+        lll_send_info("add sum: ",lll_sum);
+        lll_send_info("add ovf: ",(lll_ovf ? 255 : 0));
+    #endif
+
+    LLL_save_mem(lll_reg, (uint8_t)lll_sum);
+    LLL_save_mem(LLL_getFlagNumber('O'), (lll_ovf ? 255 : 0));
+
+    return inst_err;
+}
+
+lll_err LLL_addi(void){
+    lll_err inst_err;
+    inst_err.status=LLL_OK;
+
+    uint32_t lll_reg;
+    uint16_t lll_sum=0;
+
+    #if LLL_DEBUG_MODE
+        LLL_CHECK_REG(inst_err);
+        lll_send_info("addi reg mode: ",LLL_REG_MODE);
+        lll_reg=LLL_load_reg_addr(LLL_REG_MODE);
+        lll_sum=LLL_load_mem(lll_reg)+lll_get();
+        lll_send_info("addi sum: ",((lll_sum>256) ? (lll_sum-255) : lll_sum));
+        lll_send_info("add ovf: ",((lll_sum>256) ? 255 : 0));
+    #else
+        lll_h8=lll_get();
+        lll_reg=LLL_load_reg_addr(LLL_REG_MODE);
+        lll_sum=LLL_load_mem(lll_reg)+lll_get();
+    #endif
+
+    if(lll_sum>255){
+        lll_sum-=255;
+        LLL_save_mem(LLL_getFlagNumber('O'), 255);
+    }else{
+        LLL_save_mem(LLL_getFlagNumber('O'), 0);
+    }
+    LLL_save_mem(lll_reg, (uint8_t)lll_sum);
+
+    return inst_err;
+}
+
+lll_err LLL_sub(void){
+    lll_err inst_err;
+    inst_err.status=LLL_OK;
+
+    return inst_err;
+}
+lll_err LLL_subi(void){
+    lll_err inst_err;
+    inst_err.status=LLL_OK;
+
+    return inst_err;
+}
+
+lll_err LLL_seri(void){
+lll_err inst_err;
+
+uint32_t lll_reg; //adress of register
+
+inst_err.status=LLL_OK;
+
+//main instruction code
+
+#if LLL_DEBUG_MODE==1
+    lll_number=lll_get(); //load number
+    lll_send_info("seri number: ",lll_number); //send debug info
+    LLL_CHECK_REG(inst_err); //check if properly written
+    lll_send_info("seri reg mode: ",LLL_REG_MODE); //send debug info
+    lll_reg=LLL_load_reg_addr(LLL_REG_MODE); //load register adress
+    lll_send_info("seri adress: ",lll_reg); //send debug info
+#else
+    lll_number=lll_get(); //load number
+    lll_h8=lll_get(); //load mode of register
+    lll_reg=LLL_load_reg_addr(LLL_REG_MODE); //load adress
+#endif // LLL_DEBUG_MODE
+for(int i=0;i<lll_number;i++){
+    lll_h8=lll_get();
+    #if LLL_DEBUG_MODE
+        lll_send_info("seri value: ",lll_h8);
+    #endif
+    LLL_save_mem(lll_reg+i,lll_h8);
+}
+/////////////////////////////////////////////
+return inst_err;
+}
+
+lll_err LLL_out(void){
+    lll_err inst_err;
+    inst_err.status=LLL_OK;
+    
+    inst_err = lll_stream_out(LLL_get32bit(),LLL_load_mem(LLL_getFlagNumber('A')));
+
+    return inst_err;
+}
---
+++ LLL_functions.h
@@ -0,0 +1,14 @@
+#ifndef LLL_FUNCTIONS_H
+#define LLL_FUNCTIONS_H
+
+#include "LLL.h"
+
+lll_err LLL_add(void);
+lll_err LLL_addi(void);
+lll_err LLL_sub(void);
+lll_err LLL_subi(void);
+
+lll_err LLL_seri(void);
+lll_err LLL_out(void);
+
+#endif
