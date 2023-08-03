47031c64d649273aed611edb48ee16c4c6d18bcc
Halmon <halmoncelman@gmail.com>
2023-08-03 13:07:56 +0200

    Sub and subi added

--- LLL_functions.c
+++ LLL_functions.c
@@ -78,11 +78,69 @@ lll_err LLL_sub(void){
     lll_err inst_err;
     inst_err.status=LLL_OK;
 
+    uint8_t lll_reg;
+    uint8_t lll_ovf=0;
+    int16_t lll_sum;
+
+    lll_number=lll_get();
+
+    #if LLL_DEBUG_MODE
+    lll_send_info("sub number: ",lll_number);
+    LLL_CHECK_REG(inst_err);
+    lll_send_info("sub reg mode: ",LLL_REG_MODE);
+    lll_reg = LLL_load_reg_addr(LLL_REG_MODE);
+    #else
+    lll_h8 = lll_get();
+    lll_reg = LLL_load_reg_addr(LLL_REG_MODE);
+    #endif
+
+    lll_sum = LLL_load_mem(lll_reg);
+
+    for(int i=1;i<lll_number;i++){
+        lll_sum -= LLL_load_mem(lll_reg + i);
+        if(lll_sum < -255){
+            lll_sum += 255;
+            lll_ovf = 1;
+        }
+    }
+
+    #if LLL_DEBUG_MODE
+        lll_send_info("sub reg value: ",(lll_ovf ? lll_sum : 0));
+        lll_send_info("sub ovf: ",(lll_ovf ? 255 : lll_sum));
+    #endif
+
+    LLL_save_mem(lll_reg,(lll_ovf ? lll_sum : 0));
+    LLL_save_mem(LLL_getFlagNumber('O'),(lll_ovf ? 255 : lll_sum));
+
     return inst_err;
 }
+
+
 lll_err LLL_subi(void){
     lll_err inst_err;
     inst_err.status=LLL_OK;
+
+    uint8_t lll_reg;
+    int16_t lll_sum;
+
+    #if LLL_DEBUG_MODE
+    LLL_CHECK_REG(inst_err);
+    lll_send_info("subi reg mode: ",LLL_REG_MODE);
+    lll_reg = LLL_load_reg_addr(LLL_REG_MODE);
+    #else
+    lll_h8 = lll_get();
+    lll_reg = LLL_load_reg_addr(LLL_REG_MODE);
+    #endif
+
+    lll_sum = LLL_load_mem(lll_reg) - lll_get();
+
+    #if LLL_DEBUG_MODE
+        lll_send_info("subi reg value: ",((lll_sum < 0) ? 0 : lll_sum));
+        lll_send_info("subi ovf: ",((lll_sum < 0) ? lll_sum : 0));
+    #endif
+
+    LLL_save_mem(lll_reg,((lll_sum < 0) ? 0 : lll_sum));
+    LLL_save_mem(LLL_getFlagNumber('O'),((lll_sum < 0) ? lll_sum : 0));
 
     return inst_err;
 }
